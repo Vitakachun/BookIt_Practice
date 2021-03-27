3173c01fda1e5ff0e95eac75b95070720c4b1d8b
Vitakachun <kachunvita@gmail.com>
2021-03-27 15:38:24 -0500

    created utilities

---
+++ src/test/java/com/BookIt/runners/TestRunner.java
@@ -0,0 +1,18 @@
+package com.BookIt.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/BookIt/step_defenitions"
+
+
+)
+public class TestRunner {
+
+
+}
